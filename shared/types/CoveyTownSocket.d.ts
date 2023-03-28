b73d2fb2ddab6286cb281b2547d3a7001a9a03ed
Jess van de Ven <vandeven.j@northeastern.edu>
2023-03-27 22:10:08 -0400

    removed pollupdate nonsense

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -95,13 +95,11 @@ export interface ServerToClientEvents {
   townSettingsUpdated: (update: TownSettingsUpdate) => void;
   townClosing: () => void;
   chatMessage: (message: ChatMessage) => void;
-  pollUpdate: (update: Poll) => void;
   interactableUpdate: (interactable: Interactable) => void;
 }
 
 export interface ClientToServerEvents {
   chatMessage: (message: ChatMessage) => void;
-  pollUpdate: (update: Poll) => void;
   playerMovement: (movementData: PlayerLocation) => void;
   interactableUpdate: (update: Interactable) => void;
 }
