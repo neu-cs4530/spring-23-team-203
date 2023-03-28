5819fd061449502424762525ffc5f51e467d27a1
Danish Farooq <farooq.d@northeastern.edu>
2023-03-27 20:01:42 -0400

    refactor Poll.ts so that it meshes with anonymization of polls

--- townService/src/town/Poll.test.ts
+++ townService/src/town/Poll.test.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { mock, mockClear } from 'jest-mock-extended';
 import { nanoid } from 'nanoid';
 import Player from '../lib/Player';
@@ -11,26 +12,31 @@ describe('Poll', () => {
   let testPoll: Poll;
   const townEmitter = mock<TownEmitter>();
   let newPlayer: Player;
-  const pollId = nanoid();
   const question = 'What is the best CS class?';
   const options = ['CS4530', 'CS3300', 'CS3000', 'CS2500'];
   const creatorId = nanoid();
-  const dateCreated = new Date();
-  const votes = [['jess', 'danish'], [], ['tingwei'], ['david']];
+  const creator = { name: 'jess', id: creatorId };
+  const votes = [['jess', 'danish'], [], ['tingwei'], ['david']].map(option =>
+    option.map(voter => ({ id: randomUUID(), name: voter })),
+  );
   const settings: PollSettings = { anonymize: false, multiSelect: false };
 
   beforeEach(() => {
     mockClear(townEmitter);
-    testPoll = new Poll(
-      { pollId, creatorId, question, options, settings, dateCreated, votes },
-    );
+    testPoll = new Poll(creator, question, options, settings);
     newPlayer = new Player(nanoid(), mock<TownEmitter>());
   });
 
   describe('Poll class', () => {
     it('getVoters returns list of unique voters', () => {
+
       const testVoters = ['jess', 'danish', 'tingwei', 'david'];
-      expect(testPoll.getVoters()).toEqual(testVoters);
+      testPoll.addVote({ id: 'jess', name: 'jess' }, [0]);
+      testPoll.addVote({ id: 'danish', name: 'danish' }, [0]);
+      testPoll.addVote({ id: 'tingwei', name: 'tingwei' }, [2]);
+      testPoll.addVote({ id: 'david', name: 'david' }, [3]);
+      
+      expect(testPoll.getVoters().map(voter => voter.name)).toEqual(testVoters);
     });
   });
 });
