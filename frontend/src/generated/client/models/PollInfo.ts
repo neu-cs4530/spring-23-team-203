9ad4a6cddb108334c00e6c8a6eb3efa007f50c89
tiingweii-shii <57331456+tiingweii-shii@users.noreply.github.com>
2023-03-29 18:24:01 -0400

    merge fix

--- frontend/src/generated/client/models/PollInfo.ts
+++ frontend/src/generated/client/models/PollInfo.ts
@@ -2,12 +2,12 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export type GetAllPollsResponseItem = {
+export type PollInfo = {
     pollId: string;
     creatorId: string;
+    creatorName: string;
     question: string;
+    options: Array<string>;
     voted: boolean;
-    createdOn: string;
-    totalVotes: number;
 };
 
